17eec0cab3e64280f70ded614a32bedc023b018b
tarun1git <tarunrajput7959@gmail.com>
2022-11-15 02:07:16 +0530

    function call

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// -----------------skills-------------
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -11,7 +12,7 @@ function opentab(tab) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tab).classList.add("active-tab");
 }
-
+// --------------responsive sidemenu---------------
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
@@ -20,3 +21,20 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
+// ---------------contactform send to googlesheet-------------
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwU6A5IsTXactP4zF_8Tk-DcJRVCbS5rzbtWSbP31b8Mqeqvoqyh-bmw9-aGABJONfMHQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const deliverymsg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            deliverymsg.innerHTML = "Thank You! Your Message has been sent"
+            setTimeout(function () {
+                deliverymsg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
